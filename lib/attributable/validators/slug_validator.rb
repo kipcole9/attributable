1c958bb8ae7bd9276497613bdb977ded95f70ea1
Kip Cole <kipcole9@gmail.com>
2014-09-27 14:26:55 +0800

    Allow slugs to start with digit(s)

--- lib/attributable/validators/slug_validator.rb
+++ lib/attributable/validators/slug_validator.rb
@@ -1,5 +1,5 @@
 class SlugValidator < ActiveModel::EachValidator
-  SLUG_REGEXP = /\A[a-zA-Z][a-zA-Z0-9\-]+\Z/
+  SLUG_REGEXP = /\A[a-zA-Z0-9\-]+\Z/
   
   def validate_each(object, attribute, value)
     configuration = { :message => I18n.t("activerecord.errors.messages.slugable") }
